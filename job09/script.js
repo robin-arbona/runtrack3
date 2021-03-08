62128d82163fe79a6d8397e55c4c0c6fbc927385
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-03-08 16:32:27 +0100

    job09 without system function

--- job09/script.js
+++ job09/script.js
@@ -6,8 +6,48 @@ function tri(numbers,order){
     }
 }
 
-const numbers = [ 1 , 2 , 5 , 19 , 20 , 12 , 24, 3];
-const order = 'asc';
+function tri2(numbers,order){
+    if(order == 'asc'){
+        return sort(numbers);
+    } else if (order == 'desc'){
+        return reverse(sort(numbers));
+    }
+}
 
-console.log(tri(numbers,order));
-console.log(tri(numbers,'desc'));
+function sort(array){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] > array[i+1]){
+            tmp = array[i+1];
+            array[i+1] = array[i];
+            array[i] = tmp;
+            sort(array);
+        }
+    }
+    return array;
+}
+
+function reverse(array){
+    let arrayReverse = [];
+    for (let i = array.length; i > 0 ; i--) {
+        arrayReverse.push(array[i]);
+    }
+    return arrayReverse;
+}
+
+function arrayrandom(maxvalue,lengtharray){
+    let array = [];
+    for (let i = 0; i < lengtharray; i++) {
+        array.push(Math.trunc(Math.random()*maxvalue));
+    }
+    return array;
+}
+
+const numbers = arrayrandom(1000,10000);
+
+
+console.log(Date.now());
+console.log(tri(numbers,'asc'));
+console.log(Date.now());
+
+console.log(tri2(numbers,'asc'));
+console.log(Date.now());
