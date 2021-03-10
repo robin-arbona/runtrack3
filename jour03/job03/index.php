8a15d05d5ad9482c5770b648d095bdc5f9bd3a41
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-03-10 16:09:28 +0100

    job03 is done

---
+++ jour03/job03/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="board" style="display: grid; grid-template-columns: repeat(3, 1fr); width: 10px"></div>
+    <button id="button">Recommencer</button>
+    <script src="jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
---
+++ jour03/job03/script.js
@@ -0,0 +1,96 @@
+$(document).ready(()=>{
+    init();
+}
+)
+
+let allMoveAllowed = true;
+
+function init(){
+    $('button').click(()=>{
+        $("#board").empty();
+        init();
+    })
+    for (let i = 1; i <= 9; i++) {
+        $(`<div style="height:100px;width:100px;" id="case${i}"></div>`).appendTo("#board");
+    }
+    let rdTable = getRandomTable(9);
+    for (let i = 0; i < 9; i++) {
+        $(`<img id="img${rdTable[i]}" src="img/${rdTable[i]}.png" style="height:100px;width:100px;">`).appendTo(`#case${i}`)
+    }
+    let cases = document.querySelector('#board').childNodes;
+    cases.forEach(element => {
+        $(element).click(function(event){
+            move(this);
+            if(checkVictory()){
+                $('#button').show();
+                if(!$('#msg').length){
+                    $('<h1 id="msg" style="color:green">Vous avez gagné !!</h1>').appendTo("body");
+                }
+                allMoveAllowed = false;
+            }
+        })
+    });
+    $('#button').hide();
+    $('#msg').hide();
+    allMoveAllowed = true;
+}
+
+function checkVictory(){
+    let cases = document.querySelector('#board').childNodes;
+    let points = 0;
+    for (let i = 0; i < cases.length; i++) {
+        const element = cases[i];
+        if(element.childNodes.length > 0){
+            let divId = Number(element.id.replace('case',''));
+            let imgId = Number(element.firstChild.id.replace('img',''));
+            if(divId == imgId){
+                points++;
+            }
+        }
+        
+    }
+    if(points == 8){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function move(element){
+    from = Number(element.id.replace('case',''));
+    to = getFreeCaseId();
+    if(isMoveAllowed(from,to) && allMoveAllowed){
+        $(element).children().first().appendTo(`#case${to}`);
+    }
+}
+
+function isMoveAllowed(from,to){
+    if((to == (from + 3)) || (to == (from - 3) || (to == (from + 1)) || (to == (from - 1)))){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function getFreeCaseId(){
+    let cases = document.querySelector('#board').childNodes;
+    let i = 0;
+    for (let i = 0; i < cases.length; i++) {
+        let element = cases[i];
+        if(element.childNodes.length == 0){
+            return (i+1);
+        }
+    }
+}
+
+function getRandomTable(length){
+    let randomTable = [];
+    while (randomTable.length<length) {
+        let rdNumber = Math.trunc(Math.random()*(length+1));
+        if(!randomTable.includes(rdNumber) && rdNumber>0){
+            randomTable.push(rdNumber);
+        }
+    }
+    return randomTable;
+}
+
