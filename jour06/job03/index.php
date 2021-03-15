544062f1543349f29d6951d14f584c933f1f1dd8
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-03-15 15:06:42 +0100

    Job03 is done

---
+++ jour06/job03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link id="style" rel="stylesheet" href="style.css">
+    <script type="text/javascript" src='script.js'></script>
+</head>
+
+<body>
+    <div class="square-div"></div>
+    <div class="square-div"></div>
+    <div class="square-div"></div>
+    <div class="square-div"></div>
+</body>
+
+</html>
---
+++ jour06/job03/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    function chooseStyle(){
+        console.log('resize');
+        if (window.matchMedia("(min-width: 1600px)").matches) {
+            document.querySelectorAll(".square-div").forEach((element)=>{
+                element.style.width = (window.innerWidth)/4 -10 + 'px';
+            }) 
+        } else if (window.matchMedia("(min-width: 768px) and (max-width: 1599px)").matches) {
+            document.querySelectorAll(".square-div").forEach((element)=>{
+                element.style.width = (window.innerWidth)/2 -10 + 'px';
+            }) 
+        } else if (window.matchMedia("(max-width: 767px)").matches) {
+            document.querySelectorAll(".square-div").forEach((element)=>{
+                element.style.width = window.innerWidth - 10 + 'px';
+            })
+        } 
+    }
+    
+    chooseStyle();
+    
+    window.addEventListener('resize',chooseStyle);
+})
+
